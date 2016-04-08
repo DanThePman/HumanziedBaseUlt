9a159a944d049700e97c15cd3fc35a5fe6074f88
DanThePman <danielvelber@yahoo.de>
2016-04-08 19:13:14 +0200

    sync

--- HumanziedBaseUlt/Main.cs
+++ HumanziedBaseUlt/Main.cs
@@ -176,7 +176,7 @@ namespace HumanziedBaseUlt
 
                     CheckUltCast(enemy, timeLeft, travelTime, aioDmg, realDelayTime);
                 }
-                else /*not enough damage at all*/
+                else /*not enough damage at all*/if (me.Level >= 6)
                 {
                     Messaging.ProcessInfo(enemy.ChampionName, Messaging.MessagingType.NotEnougDamage, aioDmg);
                 }
